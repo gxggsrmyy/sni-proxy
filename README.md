f305aef9f983d1efd2bbeb7d9864e3b8f58d693a
gogotunnel <gogotunnel@gmail.com>
2016-11-17 13:16:10 +0800

    feat: listen on specified ip address.

--- README.md
+++ README.md
@@ -3,8 +3,13 @@
 sni-proxy is an asynchronous event-driven sni proxy. It helps you create a sni proxy easily without setting up a mass dependencies. All you need is Java8 environment.
 
 # Setup & Run
+Listen on all network interfaces:
 ```
 nohup java -Xmx100m -jar sni-proxy.jar  &>/dev/null &
+```
+Or listen on specified ip address:
+```
+nohup java -Xmx100m -jar sni-proxy.jar 127.0.0.1  &>/dev/null &
 ```
 Modify your local hosts file, point www.google.com to your sni proxy ip:
 ```
--- src/main/java/sni/SniProxyServer.java
+++ src/main/java/sni/SniProxyServer.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.NetUtil;
 import util.Console;
 
 public class SniProxyServer {
@@ -15,11 +16,17 @@ public class SniProxyServer {
         b.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new SniProxyServerInitializer());
-        b.bind("0.0.0.0", 443).addListener((ChannelFuture f) -> {
-            if(f.isSuccess()){
+        ChannelFuture f;
+        if(args.length > 0 && NetUtil.isValidIpV4Address(args[0])){
+            f = b.bind(args[0], 443);
+        } else {
+            f = b.bind(443);
+        }
+        f.addListener((ChannelFuture bindFuture) -> {
+            if(bindFuture.isSuccess()){
                 Console.info("sni.SniProxyServer", "Sni Proxy start on 443 ...");
             } else {
-                Console.error("sni.SniProxyServer", "Error: " + f.cause().getMessage());
+                Console.error("sni.SniProxyServer", "Error: " + bindFuture.cause().getMessage());
                 System.exit(0);
             }
         });
